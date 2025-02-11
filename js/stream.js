2fe4b3e44efd9e2fbd1422311bba746d6c907b60
F1FansGlobal <f1mexc11@gmail.com>
2025-02-11 17:23:54 -0600

    se agrego una imagen antes y despues del stream al igual que un boton que parpadea cuando ya este el stream

---
+++ js/stream.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let video = document.getElementById("live-video");
+    let preImage = document.getElementById("pre-image");
+    let postImage = document.getElementById("post-image");
+    let liveButton = document.getElementById("live-button");
+
+    // Cuando el usuario le da play al video
+    video.addEvent
